b302f4afad5e002ec6e5e7da9734242bca12996b
bintangnaufall <bintang7735@gmail.com>
2024-01-09 20:50:37 +0700

    halaman login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,11 +5,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>BPKP Kalbar</title>
     <link rel="icon" href="asset/img/bpkp_logo.ico" type="image/x-icon" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="style.css" />  
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+
+    <link rel="stylesheet" href="{{asset('css/iziToast.min.css')}}">
+
+    <link rel="stylesheet" href={{ asset('/css/bootstrap.min.css') }} type="text/css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">s  
+
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
+      body {
+        font-family: "Open Sans", sans-serif;
+      }
+      body {
+        background: url('/img/bg-login.jpg') no-repeat center center fixed;
+        background-size: cover;
+        backdrop-filter: brightness(60%);
+      }
+
+      .login-box {
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+        padding: 20px;
+        background-color: white;
+      }
+
+      #logo {
+        width: 100px;
+        margin: 0 auto;
+        display: block;
+        margin-bottom: 15px;
+      }
+
+      .form-control {
+        border: none;
+        border-bottom: 2px solid #ddd;
+        border-radius: 0;
+      }
+
+      .shadow-bottom {
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+      }
+      .password-input-wrapper {
+        position: relative;
+      }
+
+      .toggle-password {
+        position: absolute;
+        right: 15px;
+        top: 50%;
+        transform: translateY(-50%);
+        cursor: pointer;
+      }
+
+    </style>
   </head>
   <body>
     <div class="container">
@@ -19,44 +71,129 @@
       >
         <div class="col-md-6 col-lg-4 login-box">
           <p class="text-center">Aplikasi Manajemen Surat</p>
-          <img id="logo" src="asset/img/bpkp_logo.png" alt="logo" />
+          <img id="logo" src="{{asset('img/bpkp_logo.png')}}" alt="logo" />
           <div class="text-center pb-3 bold fw-semibold">
             Badan Pengawasan Keuangan dan Pembangunan
           </div>
           <form id="loginForm">
             <div class="mb-3">
-              <label for="username" class="form-label fw-medium"
-                >Username</label
-              >
-              <input type="text" class="form-control" id="username" />
+              <label for="username" class="form-label fw-medium">
+                Username
+              </label>
+              <input type="text" class="form-control" id="username" id="username"/>
             </div>
             <div class="mb-3 pb-3">
-              <label for="password" class="form-label fw-medium"
-                >Password</label
-              >
-              <input type="password" class="form-control" id="password" />
+              <label for="password" class="form-label fw-medium">
+                Password
+              </label>
+              <div class="password-input-wrapper">
+                <input type="password" class="form-control" id="password" name="password"/>
+                <span toggle="#password" class="toggle-password" onclick="togglePasswordVisibility()">
+                  <i class="bi bi-eye-fill" id="eye" aria-hidden="true"></i>
+                </span>
+              </div>
             </div>
-            <button
-              type="button"
-              class="btn btn-primary w-100"
-              onclick="attemptLogin()"
-            >
+            <button type="button" class="btn btn-primary w-100" id="btnlogin">
               LOGIN
             </button>
           </form>
-          <div class="mt-3" id="successAlert" style="display: none">
-            <div class="alert alert-success" role="alert">Login Berhasil!</div>
-          </div>
-          <div class="mt-3" id="errorAlert" style="display: none">
-            <div class="alert alert-danger" role="alert">
-              Login Gagal! Coba cek username and password.
-            </div>
-          </div>
         </div>
       </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/iziToast.min.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/script.js"></script>  
+
+    {{-- <script src="{{asset('js/script.js')}}"></script>   --}}
+    <script>
+          function togglePasswordVisibility() {
+              var passwordInput = $('#password');
+              var eyeIcon = $('#eye');
+
+              if (passwordInput.attr('type') === "password") {
+                  passwordInput.attr('type', 'text');
+                  eyeIcon.removeClass('bi-eye-fill');
+                  eyeIcon.addClass('bi-eye-slash-fill');
+              } else {
+                  passwordInput.attr('type', 'password');
+                  eyeIcon.removeClass('bi-eye-slash-fill');
+                  eyeIcon.addClass('bi-eye-fill');
+              }
+          }
+
+          $(document).ready(function () {
+              var csrfToken = $('meta[name="csrf-token"]').attr('content');
+                  $.ajaxSetup({
+                      headers: {
+                      'X-CSRF-TOKEN': csrfToken
+                      },
+                  });
+  
+                  let openToasts = 0;
+  
+                  function showToast( jenis, title, message) {   
+                      if (openToasts < 3) {
+                          iziToast[jenis]({
+                              title: title,
+                              message: message,
+                              position: "topCenter",
+                              timeout: 3000,
+                              onOpening: function () {
+                                  openToasts++;
+                              },
+                              onClosing: function () {
+                                  openToasts--;
+                              }
+                          });
+                      }
+                  }
+  
+              $('#btnlogin').click(function (e) {
+                  e.preventDefault();
+  
+                  var username = $("#username").val();
+                  var password = $("#password").val();
+                  var token = csrfToken;
+  
+                  if(username.length == "") {
+                      showToast("warning", "Oops..", "Please enter your <b>username</b>");
+                  } else if(password.length == "") {
+                      showToast("warning", "Oops..", "please enter your <b>password</b>");
+                  } else {
+                      $.ajax({
+                      url: '',
+                      type: "POST",
+                      dataType: "JSON",
+                      cache: false,
+                      data: {
+                          "username": username,
+                          "password": password,
+                          "_token": token
+                      },
+                      success:function(response){
+                          if (response.success) {
+                              iziToast.success({
+                                  title: "Login Success",
+                                  message: response.success,
+                                  timeout: 2000,
+                                  position: "topCenter",
+                                  closeOnClick: true,
+                                  onClosing: function () {
+                                      window.location.href = '/dashboard';
+                                  }
+                              })
+                          } else if (response.error) {
+                              showToast("error", "Login Error", response.error);
+                          }
+                      },
+                      error:function(error){
+                          showToast("error", "Login Error", "something went wrong");
+                      }
+                  });
+                  }
+              });
+          });
+  </script>
   </body>
 </html>
 
