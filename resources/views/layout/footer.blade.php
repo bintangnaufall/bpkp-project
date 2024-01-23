e790ea0366a84706b20487cbaa6f1652ff5f43d0
penoFahmi <penofahmi@gmail.com>
2024-01-23 18:03:21 +0700

    warne tabel

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,17 +1,17 @@
 <div class="container-fluid pt-4 px-4">
-    <div class="bg-secondary rounded-top p-4">
+    <div class="bg-white shadow-lg rounded-top p-4">
         <div class="row">
             <div class="col-12 col-sm-6 text-center text-sm-start">
-                &copy; <a href="#" class="text-white">Bintang Ganteng</a>, All
+                &copy; <a href="#" class="text-dark">Bintang Ganteng</a>, All
                 Right Reserved.
             </div>
             <div class="col-12 col-sm-6 text-center text-sm-end">
                 Designed By
-                <a href="https://rachmatwahid.com" class="text-white"
+                <a href="https://rachmatwahid.com" class="text-dark"
                 >Bintang Ganteng</a
                 >
                 <br />Distributed By:
-                <a href="#" target="_blank" class="text-white">BudaUMP</a>
+                <a href="#" target="_blank" class="text-dark">BudaUMP</a>
             </div>
         </div>
     </div>
