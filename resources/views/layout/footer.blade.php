38d1ec40bbd590c3c416b84158968bfc1958ac3a
bintangnaufall <bintang7735@gmail.com>
2024-03-29 20:40:10 +0700

    footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,18 +1,18 @@
 <div class="container-fluid pt-4 px-4">
     <div class="bg-white shadow-lg rounded-top p-4">
         <div class="row">
-            <div class="col-12 col-sm-6 text-center text-sm-start">
-                &copy; <a href="#" class="text-dark">Bintang Ganteng</a>, All
+            <div class="col-12 col-sm-12 text-center text-sm-start">
+                &copy; <a href="#" class="text-dark">-Perwakilan BPKP Provinsi Kalimantan Barat-2024</a>, All
                 Right Reserved.
             </div>
-            <div class="col-12 col-sm-6 text-center text-sm-end">
+            {{-- <div class="col-12 col-sm-6 text-center text-sm-end">
                 Designed By
                 <a href="https://rachmatwahid.com" class="text-dark"
                 >Bintang Ganteng</a
                 >
                 <br />Distributed By:
                 <a href="#" target="_blank" class="text-dark">BudaUMP</a>
-            </div>
+            </div> --}}
         </div>
     </div>
 </div>
