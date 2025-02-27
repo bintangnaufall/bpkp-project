8ae09a0d63a0ad99d92e9817d0645863e0504fa6
bintangnaufall <bintang7735@gmail.com>
2025-02-27 14:05:11 +0700

    memperbarui tampilan login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 class LoginController extends Controller
@@ -16,13 +17,27 @@ class LoginController extends Controller
         return view("login");
     }
 
+    protected function validator($data)
+    {
+        $rules = [
+            'nip' => 'required',
+            'password' => 'required'
+        ];
+
+        $message = [
+            'nip.required' => 'NIP tidak boleh kosong',
+            'password.required' => 'Password tidak boleh kosong'
+        ];
+
+        return Validator::make($data, $rules, $message);
+    }
     public function authenticate(Request $request) 
     {
 
-        $request->validate([
-            'nip' => 'required',
-            'password' => 'required',
-        ]);
+        $validator = $this->validator($request->all());
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
 
         $nip = str_replace(' ', '',$request->nip);
         
@@ -54,53 +69,5 @@ class LoginController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/');   
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -42,7 +42,7 @@
       }
 
       #logo {
-        width: 100px;
+        width: 150px;
         margin: 0 auto;
         display: block;
         margin-bottom: 15px;
@@ -69,6 +69,51 @@
         transform: translateY(-50%);
         cursor: pointer;
       }
+      input {
+        border: none;
+        background: none;
+        padding: 0;
+        margin: 0;
+        outline: none;
+        box-shadow: none;
+      }
+
+      .input-form {
+        border: #D3D9DE solid 2px; 
+        height: 40px;
+        padding-left: 7px;
+        border-top-left-radius: 5px;
+        border-bottom-left-radius: 5px;
+        background: #E8F0FE;
+      }
+      .checkbox-form {
+        width: 20px;
+        height: 20px;
+      }
+
+      .checkbox-form {
+        appearance: none; /* Hapus style bawaan */
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        
+        width: 20px;
+        height: 20px;
+        border: 2px solid #D3D9DE; /* Warna border */
+        background-color: transparent; /* Background transparan */
+        display: inline-block;
+        cursor: pointer;
+        position: relative;
+      }
+      
+      .checkbox-form:checked::before {
+        content: "✔"; /* Simbol centang */
+        font-size: 16px;
+        color: black; /* Warna centang biru */
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+      }
 
     </style>
   </head>
@@ -78,11 +123,13 @@
         class="row justify-content-center align-items-center"
         style="height: 100vh"
       >
-        <div class="col-md-6 col-lg-4 login-box">
-          <p class="text-center">Aplikasi Manajemen Surat</p>
-          <img id="logo" src="{{asset('img/bpkp_logo.png')}}" alt="logo" />
-          <div class="text-center pb-3 bold fw-semibold">
-            Badan Pengawasan Keuangan dan Pembangunan
+        <div class="col-md-3 col-lg-4 login-box" style="border-top: 4px solid #0d6efd">
+          {{-- <p class="text-center">Aplikasi Manajemen Surat</p> --}}
+          <img id="logo" src="{{asset('img/bpkp_logo.png')}}" alt="logo"/>
+          <div class="text-center pb-4 bold fw-semibold d-flex align-items-center justify-content-center" style="border-bottom: 2px solid #0d6efd">
+            <div class="col-md-9">
+              Badan Pengawasan Keuangan dan Pembangunan
+            </div> 
           </div>
 
           @if(session()->has('loginError'))
@@ -97,34 +144,48 @@
             </script>
           @endif
 
-          <form id="loginForm" action="/login" method="post">
+          <div class="bold text-center fw-semibold">
+            <p class="m-0 pt-2">Aplikasi Manajemen Surat</p>
+            {{-- <p>Halaman Login</p> --}}
+          </div>
+
+          <form id="loginForm" action="/login" method="post" class="mt-3">
             @csrf
             <div class="mb-3">
-              <label for="nip" class="form-label fw-medium">
-                NIP
-              </label>
-              <input type="text" class="form-control block-mask" id="nip" name="nip" value="{{ old('nip') }}" autofocus/>
+              <div class="d-flex">
+                <input type="text" class="input-form w-100" id="nip" name="nip" placeholder="NIP" value="{{ old('nip') }}" autofocus/>
+                <div class="d-flex justify-content-center" style="background-color:#E9ECEF; width:40px; border-top:#D3D9DE solid 2px; border-right:#D3D9DE solid 2px; border-bottom:#D3D9DE solid 2px; border-top-right-radius: 5px; border-bottom-right-radius: 5px;">
+                  <i class="bi bi-person-fill" style="font-size: 24px"></i>
+                </div>
+              </div>
               @error('nip')
-                <small style="color:red;">{{ $message }} </small>
+                <small style="color:red;" class="bold fw-semibold">{{ $message }}</small>
               @enderror
             </div>
             
-            <div class="mb-3 pb-3">
-              <label for="password" class="form-label fw-medium">
-                Password
-              </label>
-              <div class="password-input-wrapper">
+            <div class="mb-3">
+              <div class="d-flex">
+                <input type="password" class="input-form w-100" id="password" placeholder="Password" name="password" value="{{ old('password') }}"/>
+                <div class="d-flex justify-content-center" style="background-color:#E9ECEF; width:40px; border-top:#D3D9DE solid 2px; border-right:#D3D9DE solid 2px; border-bottom:#D3D9DE solid 2px; border-top-right-radius: 5px; border-bottom-right-radius: 5px;">
+                  <i class="bi bi-lock-fill" style="font-size: 24px"></i>
+                </div>
+              </div>
+              @error('password')
+                <small style="color:red;" class="bold fw-semibold">{{ $message }}</small>
+              @enderror
+              {{-- <div class="password-input-wrapper">
                 <input type="password" class="form-control" id="password" name="password" value="{{ old('password') }}"/>
-                @error('password')
-                  <small style="color:red;">{{ $message }}</small>
-                @enderror
                 <span toggle="#password" class="toggle-password" onclick="togglePasswordVisibility()">
                   <i class="bi bi-eye-fill" id="eye" aria-hidden="true"></i>
                 </span>
-              </div>
+              </div> --}}
             </div>
-            <button type="submit" class="btn btn-info w-100" id="btnlogin">
-              LOGIN
+            <div class="d-flex mb-2">
+              <input type="checkbox" class="checkbox-form me-1" id="showPass">
+              <p style="margin-top: -2.5px">Tampilkan kata sandi</p>
+            </div>
+            <button type="submit" class="btn btn-primary w-100 text-white" id="btnlogin">
+              Masuk
             </button>
           </form>
         </div>
@@ -138,20 +199,49 @@
     <script src="{{ asset('assets/js/forms-extras.js')}}"></script>
 
     <script>
-          function togglePasswordVisibility() {
-              var passwordInput = $('#password');
-              var eyeIcon = $('#eye');
-
-              if (passwordInput.attr('type') === "password") {
-                  passwordInput.attr('type', 'text');
-                  eyeIcon.removeClass('bi-eye-fill');
-                  eyeIcon.addClass('bi-eye-slash-fill');
-              } else {
-                  passwordInput.attr('type', 'password');
-                  eyeIcon.removeClass('bi-eye-slash-fill');
-                  eyeIcon.addClass('bi-eye-fill');
-              }
+      $(document).ready(function () {
+        $("#showPass").on("change", function () {
+          if ($(this).prop("checked")) {
+            $("#password").attr("type", "text");
+          }else {
+            $("#password").attr("type", "password");
           }
+        });
+
+        $("#nip").on("input", function () {
+          $(this).val($(this).val().replace(/\D/g, "")); // Hapus semua karakter selain angka
+        });
+        $("#nip").on("input", function () {
+          let value = $(this).val().replace(/\s/g, ""); // Hapus spasi yang sudah ada
+          if (value.length > 8) {
+            value = value.slice(0, 8) + " " + value.slice(8); // Tambahkan spasi setelah 8 digit
+          }
+          if (value.length > 15) {
+            value = value.slice(0, 15) + " " + value.slice(15); // Tambahkan spasi setelah 8 digit
+          }
+          if (value.length > 17) {
+            value = value.slice(0, 17) + " " + value.slice(17); // Tambahkan spasi setelah 8 digit
+          }
+          if (value.length > 21) { // Maksimal 18 angka (karena 21 termasuk spasi)
+            value = value.slice(0, 21); // Potong agar tidak lebih dari 18 angka
+          }
+          $(this).val(value);
+        });
+      });
+          // function togglePasswordVisibility() {
+              // var passwordInput = $('#password');
+              // var eyeIcon = $('#eye');
+
+              // if (passwordInput.attr('type') === "password") {
+              //     passwordInput.attr('type', 'text');
+              //     eyeIcon.removeClass('bi-eye-fill');
+              //     eyeIcon.addClass('bi-eye-slash-fill');
+              // } else {
+              //     passwordInput.attr('type', 'password');
+              //     eyeIcon.removeClass('bi-eye-slash-fill');
+              //     eyeIcon.addClass('bi-eye-fill');
+              // }
+          // }
           // $(document).ready(function () {
           //     var csrfToken = $('meta[name="csrf-token"]').attr('content');
           //         $.ajaxSetup({
